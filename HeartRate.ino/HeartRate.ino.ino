4bc2833414ecae4d680032135acb8ea21eba2d5a
Benbigmac <benmaciorowski@yahoo.com>
2019-03-10 19:14:04 -0500

    INITIAL COMMIT WITH CODE FOR BLUEfruit as well as labright up and image of sensor locations

---
+++ HeartRate.ino/HeartRate.ino.ino
@@ -0,0 +1,123 @@
+
+#include <Wire.h>
+#include <SPI.h>
+
+#if defined(ARDUINO_ARCH_SAMD)
+// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+   #define Serial SerialUSB
+#endif
+#include <Arduino.h>
+#include <SPI.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+#include "pitches.h"
+#include "BluefruitConfig.h"
+
+#if SOFTWARE_SERIAL_AVAILABLE
+  #include <SoftwareSerial.h>Ω
+#endif
+
+int heartRateReading=A0;
+int Respiration=A1;
+int buzzerPin = 9;
+int anoyingSOund = 8;
+int heartRate=0;
+
+int melody[] = {
+  NOTE_E7, NOTE_E7, NOTE_F7, NOTE_G7, NOTE_G7, NOTE_F7, NOTE_E7, NOTE_DS7, NOTE_C7, NOTE_C7, NOTE_D7, NOTE_E7, NOTE_E7, NOTE_DS7, NOTE_DS7,0
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 4, 4, 4, 4, 4, 4, 4,4, 4, 4, 4, 2, 4
+};
+
+    #define FACTORYRESET_ENABLE         0
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+    #define MODE_LED_BEHAVIOUR          "MODE"
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+    
+void setup() {
+  // initialize the serial communication:
+  Serial.begin(9600);
+  pinMode(10, INPUT); // Setup for leads off detection LO +
+  pinMode(11, INPUT); // Setup for leads off detection LO -
+  pinMode(buzzerPin, INPUT); //Gives you a buzz when you a little too paniced
+  pinMode(anoyingSOund, INPUT);
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+ // Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+ // Serial.println(F("Then Enter characters to send to Bluefruit"));
+ // Serial.println();
+  ble.verbose(false);  // debug info is a little annoying after this point!
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+  Serial.println(F("******************************"));
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+  // Set module to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+  Serial.println(F("******************************"));
+}
+
+void loop() {
+  
+  if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
+    Serial.println('!');
+  }
+  else{
+    // send the value of analog input 0:
+     heartRate=analogRead(A0);
+     Serial.println(heartRate);
+     ble.print("heartRate\n");
+     if(heartRate>60){
+        for (int thisNote = 0; thisNote < 15; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+     }{
+      tone(8, melody[thisNote]);
+     }
+      
+  }
+  //Wait for a bit to keep serial data from saturating
+  delay(1);
+}
