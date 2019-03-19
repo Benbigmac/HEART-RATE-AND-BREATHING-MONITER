a1e0ff0b966f57aa68866f0b0323e9b5ddb30bcb
cmondragon15117 <cmondragon15117@gmail.com>
2019-03-18 20:15:20 -0500

    add led meditation rate calcs

--- HeartRate.ino/HeartRate.ino.ino
+++ HeartRate.ino/HeartRate.ino.ino
@@ -4,32 +4,44 @@
 
 #if defined(ARDUINO_ARCH_SAMD)
 // for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
-   #define Serial SerialUSB
+#define Serial SerialUSB
 #endif
 #include <Arduino.h>
 #include <SPI.h>
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
-#include "pitches.h"
+//#include "pitches.h"
 #include "BluefruitConfig.h"
 
 #if SOFTWARE_SERIAL_AVAILABLE
-  #include <SoftwareSerial.h>Ω
+#include <SoftwareSerial.h>Ω
 #endif
 
 int heartRateReading=A0;
 int Respiration=A1;
 int buzzerPin = 9;
 int lightUp = 8;
-int heartRate=0;
-
-
-
-    #define FACTORYRESET_ENABLE         0
-    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
-    #define MODE_LED_BEHAVIOUR          "MODE"
+int heartRate = 0;
+#define FACTORYRESET_ENABLE         0
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+
+
+//Variables for LED respiration rate
+int ledPin = 13;
+int upperThreshold = 495; 
+int lowerThreshold = 490;
+int meditationThreshold = 18; //rate per min that is considered for meditation
+int reading = 0;
+float rate = 0.0;
+bool ignoreReading = false;
+bool firstPulseDetected = false;
+unsigned long firstPulseTime = 0;
+unsigned long secondPulseTime = 0;
+unsigned long pulseInterval = 0;
 
 void error(const __FlashStringHelper*err) {
   Serial.println(err);
@@ -39,6 +51,7 @@ void error(const __FlashStringHelper*err) {
 void setup() {
   // initialize the serial communication:
   Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
   pinMode(10, INPUT); // Setup for leads off detection LO +
   pinMode(11, INPUT); // Setup for leads off detection LO -
   pinMode(buzzerPin, INPUT); //Gives you a buzz when you a little too paniced
@@ -65,13 +78,13 @@ void setup() {
   /* Print Bluefruit information */
   ble.info();
 
- // Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
- // Serial.println(F("Then Enter characters to send to Bluefruit"));
- // Serial.println();
+  // Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  // Serial.println(F("Then Enter characters to send to Bluefruit"));
+  // Serial.println();
   ble.verbose(false);  // debug info is a little annoying after this point!
   /* Wait for connection */
   while (! ble.isConnected()) {
-      delay(500);
+    delay(500);
   }
   Serial.println(F("******************************"));
   // LED Activity command is only supported from 0.6.6
@@ -88,26 +101,57 @@ void setup() {
 }
 
 void loop() {
+  reading = analogRead(A1);
+  if(reading > upperThreshold && ignoreReading == false){
+    if(firstPulseDetected == false){
+      firstPulseTime = millis();
+      firstPulseDetected = true;
+    }
+    else{
+      secondPulseTime = millis();
+      pulseInterval = secondPulseTime - firstPulseTime;
+      firstPulseTime = secondPulseTime;
+    }
+    ignoreReading = true;
+  }
+
+  // Heart beat trailing edge detected.
+  if(reading < lowerThreshold){
+    ignoreReading = false;
+  }
+
+  rate = (1.0/pulseInterval) * 60.0 * 1000;
+  Serial.print("Rate is ");
+  Serial.println(rate);
+  if(rate > meditationThreshold) {
+    digitalWrite(ledPin, HIGH);
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+  
+
 
   if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
     Serial.println('!');
   }
   else{
     // send the value of analog input 0:
-     heartRate=analogRead(A2);
-     Respiration=analogRead(A1);
-     Serial.println(heartRate);
-     ble.print(Respiration);
-     ble.print(",");
-     ble.print(heartRate);
-     ble.print("\n");
-     
-   if(heartRate>180){ //need to change or calculate the heart rate
+    heartRate=analogRead(A2);
+    
+    //Respiration=analogRead(A1);
+    //Serial.println(heartRate);
+    ble.print(reading);
+    ble.print(",");
+    ble.print(heartRate);
+    ble.print("\n");
+
+    if(heartRate>180){ //need to change or calculate the heart rate
       digitalWrite(lightUp, HIGH);
-      
-   }else{
+
+    }else{
       digitalWrite(lightUp, LOW);
-   }
+    }
 
   }
   //Wait for a bit to keep serial data from saturating
